e231d31262b73f8c9bbaae93b48af8c6c6c85123
Oscar Martin Mora <moramartin99@gmail.com>
2023-08-27 15:49:43 -0600

    Actualizar getBookCardElement y rebuildBookCardElement

--- script.js
+++ script.js
@@ -602,7 +602,15 @@ const getBookCardElement = (bookObj) => {
                         />
                     </g>
                 </svg>
-                <button class="editButton">Editar</button>
+                <button class="editButton">
+                <svg class="editButtonIcon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <path
+            class="fills"
+            d="m12.9 6.854 4.242 4.243-9.9 9.9H3v-4.243l9.9-9.9Zm1.414-1.414 2.121-2.121a1 1 0 0 1 1.414 0l2.829 2.828a1 1 0 0 1 0 1.414l-2.122 2.122-4.242-4.243Z"
+        />
+    </svg>
+    Editar
+                </button>
                 <div class="infoContainer">
                     <p class="bookTitle">${bookObj.title}</p>
                     <p class="bookAuthor">${bookObj.author}</p>
@@ -692,7 +700,15 @@ const rebuildBookCardElement = (bookCardElement) => {
                         />
                     </g>
                 </svg>
-                <button class="editButton">Editar</button>
+                <button class="editButton">
+                <svg class="editButtonIcon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <path
+            class="fills"
+            d="m12.9 6.854 4.242 4.243-9.9 9.9H3v-4.243l9.9-9.9Zm1.414-1.414 2.121-2.121a1 1 0 0 1 1.414 0l2.829 2.828a1 1 0 0 1 0 1.414l-2.122 2.122-4.242-4.243Z"
+        />
+    </svg>
+    Editar
+                </button>
                 <div class="infoContainer">
                     <p class="bookTitle">${bookObj.title}</p>
                     <p class="bookAuthor">${bookObj.author}</p>
